ac23684b2d6a83ea8486ee7b1094c3d58a6bc9a1
yarastqt <yarastqt@gmail.com>
2018-03-11 01:52:30 +0300

    feat(index): make rotors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-class Enigma {
-  constructor() { }
+import { Rotor, createRotorEnchance } from './utils/create-rotor'
+import { Alphabet, alphabet } from './constants/alphabet'
+import { rotorsSettings } from './constants/rotors'
+import { Enigma } from './enigma'
+
+type Rotors = {
+  [key: number]: Rotor
 }
+
+const rotors: Rotors = {
+  1: createRotorEnchance(rotorsSettings[1]),
+  2: createRotorEnchance(rotorsSettings[2]),
+  3: createRotorEnchance(rotorsSettings[3]),
+}
+const machine = new Enigma<Rotors>(
+  rotors
+)
+
