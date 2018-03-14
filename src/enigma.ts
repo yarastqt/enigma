388cbac002eea2200c6a5ce461a3954411febc16
yarastqt <yarastqt@gmail.com>
2018-03-15 01:07:27 +0300

    feat: create rotors as classes

--- src/enigma.ts
+++ src/enigma.ts
@@ -1,7 +1,12 @@
-import { Rotor } from './utils/create-rotor'
+import { Rotor } from './utils/create-rotor-settings'
+
+type RotorSetting = {
+  rotor: number
+  position: number
+}
 
 interface IEnigma {
-  setRotors(): void
+  setRotors(settings: RotorSetting[]): void
   encode(): void
 }
 
@@ -10,14 +15,19 @@ export class Enigma<R> implements IEnigma {
   private rotor2: Rotor
   private rotor3: Rotor
 
-  constructor(rotors: R) {
+  public constructor(rotors) {
+    // TODO: add reflector
     this.rotor1 = rotors[1]
     this.rotor2 = rotors[2]
     this.rotor3 = rotors[3]
+    // this.setRotors()
   }
 
-  public setRotors() {
-
+  /**
+   * Set initial settings of rotors
+   */
+  public setRotors(settings?: RotorSetting[]) {
+    
   }
 
   public encode() {
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
-import { Rotor, createRotorEnchance } from './utils/create-rotor'
 import { Alphabet, alphabet } from './constants/alphabet'
 import { rotorsSettings } from './constants/rotors'
 import { Enigma } from './enigma'
+import { Rotor } from './rotor'
 
-type Rotors = {
-  [key: number]: Rotor
-}
-
-const rotors: Rotors = {
-  1: createRotorEnchance(rotorsSettings[1]),
-  2: createRotorEnchance(rotorsSettings[2]),
-  3: createRotorEnchance(rotorsSettings[3]),
-}
-const machine = new Enigma<Rotors>(
+// TODO: add type
+const rotors = [
+  new Rotor({ id: 1, position: 25 }),
+  new Rotor({ id: 2, position: 0 }),
+  new Rotor({ id: 3, position: 0 }),
+]
+const machine = new Enigma(
   rotors
 )
-
