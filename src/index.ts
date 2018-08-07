081a1d4279b5071949fc5dfbb51010e7580c5bab
yarastqt <yarastqt@gmail.com>
2018-08-08 00:56:13 +0300

    feat(index): add base implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import { CipherDetail, getCipherDetail } from './shared'
+import { rotors } from './rotors'
+import { reflectors } from './reflectors'
 
+
+const configureMachine = (rotors: CipherDetail[], reflector: CipherDetail) => {}
+const encrypt = (text: string, machine: any) => {}
+
+const machine = configureMachine([
+  getCipherDetail('I', rotors),
+  getCipherDetail('II', rotors),
+  getCipherDetail('III', rotors),
+], getCipherDetail('UKW-B', reflectors))
+
+const result = encrypt('hello', machine)
