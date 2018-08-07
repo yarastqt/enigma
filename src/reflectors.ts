87856441aaad06718a409055ab616e5775337e28
yarastqt <yarastqt@gmail.com>
2018-08-08 00:54:08 +0300

    feat(reflectors): add common reflectors

---
+++ src/reflectors.ts
@@ -0,0 +1,14 @@
+import { CipherDetail } from './shared'
+
+
+export const reflectors: CipherDetail[] = [
+  {
+    name: 'UKW-B',
+    label: 'UKW B',
+    wiring: [
+      'y', 'r', 'u', 'h', 'q', 's', 'l', 'd', 'p',
+      'x', 'n', 'g', 'o', 'k', 'm', 'i', 'e', 'b',
+      'f', 'z', 'c', 'w', 'v', 'j', 'a', 't',
+    ]
+  }
+]
