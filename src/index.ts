282fe6dc9d0c36eb7fc439ee7ba5332c8786750c
yarastqt <yarastqt@gmail.com>
2018-08-08 00:35:27 +0300

    refactor: remove all

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import { Alphabet, alphabet } from './constants/alphabet'
-import { rotorsSettings } from './constants/rotors'
-import { Enigma } from './enigma'
-import { Rotor } from './rotor'
 
-// TODO: add type
-const rotors = [
-  new Rotor({ id: 1, position: 25 }),
-  new Rotor({ id: 2, position: 0 }),
-  new Rotor({ id: 3, position: 0 }),
-]
-const machine = new Enigma(
-  rotors
-)
